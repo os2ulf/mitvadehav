d9230d5366e23613850d56880cc3f32eebbee1b2
Peter Vesterkær <pve@novicell.dk>
2020-06-24 10:06:26 +0200

    OS2ULF-242: Added siteimprove script

--- template.php
+++ template.php
@@ -1,1 +1,18 @@
 <?php
+
+
+function ulf_mitvadehav_preprocess_html(&$variables) {
+  drupal_add_js(
+    '/*<![CDATA[*/
+(function() {
+var sz = document.createElement(\'script\'); sz.type = \'text/javascript\'; sz.async = true;
+sz.src = \'//siteimproveanalytics.com/js/siteanalyze_133886.js\';
+var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/',
+    [
+      'type' => 'inline',
+      'scope' => 'footer',
+    ]
+  );
+}
